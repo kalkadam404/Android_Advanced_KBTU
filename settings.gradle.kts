6fa8b1b822a7923fdd4416bed17b5142a8a4a474
kalkadam404 <kalkadamov14@gmail.com>
2025-04-12 21:21:12 +0500

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
+import java.util.Properties
 pluginManagement {
     repositories {
         google()
@@ -5,11 +6,27 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+val localProperties = Properties().apply {
+    load(File(rootProject.projectDir, "local.properties").inputStream())
+}
+val mavenUsername: String = localProperties["mavenUsername"] as String
+val mavenPassword: String = localProperties["mavenPassword"] as String
+
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kalkadam404/Android_Advanced_KBTU")
+            credentials {
+                username = mavenUsername
+                password = mavenPassword
+            }
+        }
     }
 }
 
